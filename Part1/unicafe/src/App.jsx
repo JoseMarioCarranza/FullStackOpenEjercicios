90d028f0935b35109e835d38c40d8301b12b0489
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-07-29 19:34:39 -0700

    Development of Exercise 1.7

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,40 +1,68 @@
 import { useState } from "react"
 
-const RateButton = ({ variable, setState, text }) => (
-  <button onClick={() => setState(variable + 1)}>
-    {text}
-  </button>
-)
+const RateButton = ({
+  variable,
+  setState,
+  text,
+  totalVotes,
+  setTotalVotes }) => {
 
-const Statistics = ({ good, neutral, bad }) => (
+  const update = () => {
+    setState(variable + 1)
+    setTotalVotes(totalVotes + 1)
+  }
+
+  return (
+    <button onClick={update}>
+      {text}
+    </button>
+  )
+}
+
+const Statistics = ({ good, neutral, bad, totalVotes }) => (
   <div>
     <h1>statistics</h1>
     <p>good: {good}</p>
     <p>neutral: {neutral}</p>
     <p>bad: {bad}</p>
+    <p>all: {totalVotes}</p>
+    <p>average: {totalVotes == 0
+      ? 'Waiting for display'
+      : (good - bad) / totalVotes}
+    </p>
+    <p>positive: {totalVotes == 0
+      ? 'Waiting for display' :
+      (good / totalVotes) * 100} %
+    </p>
   </div>
 )
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [totalVotes, setTotalVotes] = useState(0)
 
   return (
     <div>
       <h1>give feed back</h1>
       <RateButton
+        totalVotes={totalVotes}
+        setTotalVotes={setTotalVotes}
         variable={good}
         setState={setGood}
         text='good'
       />
       <RateButton
+        totalVotes={totalVotes}
+        setTotalVotes={setTotalVotes}
         variable={neutral}
         setState={setNeutral}
         text='neutral'
       />
       <RateButton
+        totalVotes={totalVotes}
+        setTotalVotes={setTotalVotes}
         variable={bad}
         setState={setBad}
         text='bad'
@@ -43,6 +71,7 @@ const App = () => {
         good={good}
         neutral={neutral}
         bad={bad}
+        totalVotes={totalVotes}
       />
     </div>
   )
