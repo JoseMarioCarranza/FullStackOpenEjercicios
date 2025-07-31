14abf2bf882d23f831ea62dcb9ea852191f332d5
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-07-31 02:09:30 -0700

    Development of Excercise 2.7

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -9,6 +9,11 @@ const App = () => {
   const handleSubmit = event => {
     event.preventDefault()
 
+    if (persons.find(p => p.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
     const personObject = {
       name: newName
     }
