b13cd5e158d03a9ca32a81802384fb2760b8fa52
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-07-31 00:58:34 -0700

    Development of Excercise 2.2

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -5,15 +5,23 @@ const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 const Content = ({ parts }) => {
   return (
     <>
-      {parts.map(part => <Part key={part.name} part={part} />)}
+      {parts.map(part => <Part key={part.id} part={part} />)}
     </>
   )
+}
+
+const Total = ({ parts }) => {
+
+  const total = parts.reduce((t, p) => t + p.exercises, 0)
+
+  return <p>Total of exercises {total}</p>
 }
 
 const Course = ({ course }) => (
   <>
     <Header course={course.name} />
     <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </>
 )
 
