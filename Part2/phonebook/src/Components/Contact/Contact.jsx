d633a5dfc7a5bde247dc4284d6d05d061fc6c230
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-01 16:35:40 -0700

    Development of Excercise 2.14

--- Part2/phonebook/src/Components/Contact/Contact.jsx
+++ Part2/phonebook/src/Components/Contact/Contact.jsx
@@ -3,12 +3,14 @@ import personsServices from "../../services/persons"
 const Contact = ({ person, setPersons, persons }) => {
 
     const handleClick = () => {
-        personsServices
-            .trash(person.id)
-            .then(r => {
-                console.log(`person with id ${person.id} has been deleted`)
-                setPersons(persons.filter(p => p.id !== person.id))
-            })
+        if (window.confirm(`Delete ${person.name}?`)) {
+            personsServices
+                .trash(person.id)
+                .then(r => {
+                    console.log(`person with id ${person.id} has been deleted`)
+                    setPersons(persons.filter(p => p.id !== person.id))
+                })
+        }
     }
 
     return (
