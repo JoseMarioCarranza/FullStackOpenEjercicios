5afd7d06285e1e06c68a663a3f453a35d5c5f40c
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-03 15:57:28 -0700

    Development of Excercise 3.3

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -35,6 +35,15 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (!person) return res.status(404).end()
+
+    res.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`);
