7f9818f3ff21d839f7f4649e88e7b57812ea80dc
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-06 22:06:24 -0700

    Development of Excercise 3.15

--- Part3/phonebook/backend/index.js
+++ Part3/phonebook/backend/index.js
@@ -51,11 +51,10 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+    const id = req.params.id
 
-    persons = persons.filter(p => p.id !== id)
-
-    res.status(204).end()
+    Person.findByIdAndDelete(id)
+        .then(r => res.status(204).end())
 })
 
 const PORT = process.env.PORT || 3001
