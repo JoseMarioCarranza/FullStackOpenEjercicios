c4ff75ea8691f69f8103607916763c1d208abae0
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-07 19:41:18 -0700

    Development of Excercise 3.20

--- Part3/phonebook/backend/models/person.js
+++ Part3/phonebook/backend/models/person.js
@@ -18,7 +18,13 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        required: true
+        minLength: 8,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d{5,}$/.test(v);
+            }, message: props => `${props.value} is not a valid phone number!`
+        }
     }
 })
 
