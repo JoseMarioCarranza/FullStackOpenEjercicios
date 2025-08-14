cd65701d2304fb6f4b70f80e6746b4a609d76652
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-14 00:05:34 -0700

    Development of exercise 4.20

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -6,8 +6,12 @@ const blogsRoutes = require('./controllers/blogs')
 const userRoutes = require('./controllers/user')
 const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(MONGODB_URI)
+
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use(cors())
 app.use(express.json())
@@ -15,5 +19,8 @@ app.use(express.json())
 app.use('/api/blogs', blogsRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/login', loginRouter)
+
+app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
 
 module.exports = app
