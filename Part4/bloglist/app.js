fc2856df51daf64094310902263119b2a71f9ad2
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-13 19:25:46 -0700

    Development of Excercise 4.15

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRoutes = require('./controllers/blogs')
+const userRoutes = require('./controllers/user')
 const mongoose = require('mongoose')
 
 mongoose.connect(MONGODB_URI)
@@ -11,5 +12,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRoutes)
+app.use('/api/users', userRoutes)
 
 module.exports = app
---
+++ Part4/bloglist/controllers/user.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt')
+const userRoutes = require('express').Router()
+const User = require('../models/user')
+
+userRoutes.post('/', async (req, res) => {
+    const { username, name, password } = req.body
+
+    if (!username, !name, !password) {
+        const err = new Error('Some data is missing')
+        err.status = 400
+        throw err
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    const user = new User({ username, name, passwordHash })
+    const savedUser = await user.save()
+
+    res.status(201).json(savedUser)
+})
+
+userRoutes.get('/', async (req, res) => {
+    const users = await User.find({})
+
+    res.status(200).json(users)
+})
+
+module.exports = userRoutes
