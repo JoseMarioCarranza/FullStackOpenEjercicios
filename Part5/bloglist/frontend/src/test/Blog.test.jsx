f9abd3ffdca30963073cb8b6ed0b7c403c5c45a1
JoseMarioCarranza <imt_josecarranza@outlook.com>
2025-08-22 21:19:30 -0700

    Development of Excercise 5.14

--- Part5/bloglist/frontend/src/test/Blog.test.jsx
+++ Part5/bloglist/frontend/src/test/Blog.test.jsx
@@ -1,35 +1,65 @@
 import { render, screen } from '@testing-library/react'
 import Blog from '../components/Blog'
 import { expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
-test('renders content', () => {
-    const blog = {
-        title: 'The blog of vitest',
-        author: 'José Mario',
-        url: 'www.ingjosemario.com',
-        likes: '100',
-        user: {
-            username: 'Aperta'
-        },
-    }
-
-    const user = {
-        username: 'Aperta'
-    }
+describe('<Blog />', () => {
 
     let container
 
-    container = render(<Blog blog={blog} user={user} />).container
+    beforeEach(() => {
+        const blog = {
+            title: 'The blog of vitest',
+            author: 'José Mario',
+            url: 'www.ingjosemario.com',
+            likes: '100',
+            user: {
+                username: 'Aperta'
+            },
+        }
 
-    const title = screen.findByText('The blog of vitest')
-    expect(title).toBeDefined()
-    const author = screen.findByText('José Mario')
-    expect(author).toBeDefined()
+        const user = {
+            username: 'Aperta'
+        }
 
-    const shortContent = container.querySelector('.shortContent')
-    expect(shortContent).not.toHaveStyle('display: note')
+        container = render(<Blog blog={blog} user={user} />).container
 
-    const fullContent = container.querySelector('.fullContent')
-    expect(fullContent).toHaveStyle('display: none')
+    })
 
+    test('shortContent test', () => {
+        const title = screen.findByText('The blog of vitest')
+        expect(title).toBeDefined()
+        const author = screen.findByText('José Mario')
+        expect(author).toBeDefined()
+
+        const shortContent = container.querySelector('.shortContent')
+        expect(shortContent).not.toHaveStyle('display: none')
+
+        const fullContent = container.querySelector('.fullContent')
+        expect(fullContent).toHaveStyle('display: none')
+    })
+
+    test('Full content test', async () => {
+
+        const user = userEvent.setup()
+        const button = screen.getByText('view')
+        await user.click(button)
+
+        const title = screen.findByText('The blog of vitest')
+        expect(title).toBeDefined()
+        const author = screen.findByText('José Mario')
+        expect(author).toBeDefined()
+        const url = screen.findByText('www.ingjosemario.com')
+        expect(url).toBeDefined()
+        const likes = screen.findByText('100')
+        expect(likes).toBeDefined()
+        const username = screen.findByText('Aperta')
+        expect(username).toBeDefined()
+
+        const shortContent = container.querySelector('.shortContent')
+        expect(shortContent).toHaveStyle('display: none')
+
+        const fullContent = container.querySelector('.fullContent')
+        expect(fullContent).not.toHaveStyle('display: none')
+    })
 })
